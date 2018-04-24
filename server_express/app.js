03cb31b4fb169eaa1017afd821559a196c6b7d3d
Vaidehi Naik <vaidehinaik@Vaidehis-MacBook-Pro.local>
2018-04-23 22:34:01 -0700

    app js fixes

--- server_express/app.js
+++ server_express/app.js
@@ -23,8 +23,8 @@ var corSettings = {
 app.use(cors(corSettings));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
